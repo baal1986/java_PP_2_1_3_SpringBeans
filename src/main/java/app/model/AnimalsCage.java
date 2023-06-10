9899598c1fe20b115f28cd369a572d0f1006a4a2
Alexei Bakholdin <aleksey.baholdin@gmail.com>
2023-06-10 19:03:54 +0300

    add: AnimalsCage::getTimer

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,11 +1,16 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+    @Autowired
+    private Timer timer;
+
+    @Qualifier("dog")
     @Autowired
     private Animal animal;
 
@@ -13,7 +18,12 @@ public class AnimalsCage {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+
+
+    public Timer getTimer() {
+        return timer;
     }
 }
